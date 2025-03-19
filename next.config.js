8371c4513eaba4b5e3de33b346309de31b42a4ff
trippintoes <153808644+trippintoes@users.noreply.github.com>
2025-03-19 14:40:03 +0100

    Förbered för publicering på GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // För statisk export, gör det enkelt att använda som iframe
+  output: 'export',  // Ställer in att bygga till statiska filer
+  distDir: 'out',    // Specificerar out-mappen som utdatamapp
+  // Om du ska publicera på GitHub Pages på en URL som inte är rot (t.ex. username.github.io/begrepplistan)
+  basePath: '/begrepplistan',
+  images: {
+    unoptimized: true, // Behövs för export till statiska sidor
+  }
 };
 
 module.exports = nextConfig; 
