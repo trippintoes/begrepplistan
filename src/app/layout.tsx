794caa63ffede4bc73f2a815607e67a9ad1a76f9
trippintoes <153808644+trippintoes@users.noreply.github.com>
2025-05-12 09:42:29 +0200

    ändrar title
    ändrar titlen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Begrepplista - Tekniska Termer och Definitioner',
+  title: 'Nomenklaturlista - Tekniska termer och definitioner',
   description: 'En samling av tekniska begrepp, förklaringar och källor från olika standarder och specifikationer.',
 };
 
